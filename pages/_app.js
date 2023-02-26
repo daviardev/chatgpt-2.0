0c0072b1dad368d6c92001dc08040ccd341140cc
devsilva <jerson.silva99@hotmail.com>
2023-02-26 18:27:49 -0500

    feat: Add provider session

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import { SessionProvider } from 'next-auth/react'
 
-import Layout from 'components/Layout'
-
 import 'styles/globals.css'
 
 export default function App ({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </Layout>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
   )
 }
